f4a9ad35c2fd00e6235f15fa8233c449a70f6760
Paklosha <paklosha@gmail.com>
2021-06-13 23:20:38 +0300

    deploy

---
+++ precache-manifest.9b922886bcded63db5d66553752021a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09d3d70e2d46ca45400",
+    "url": "/SimonTheGame/css/app.01dcf13a.css"
+  },
+  {
+    "revision": "9e424cd72b0321d3c20f",
+    "url": "/SimonTheGame/css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "3889ece508241673cc04198f401a71ff",
+    "url": "/SimonTheGame/img/memory-lights.png"
+  },
+  {
+    "revision": "e3d736b8c020a743b0f434b15262d8ce",
+    "url": "/SimonTheGame/index.html"
+  },
+  {
+    "revision": "b09d3d70e2d46ca45400",
+    "url": "/SimonTheGame/js/app.33fd3108.js"
+  },
+  {
+    "revision": "9e424cd72b0321d3c20f",
+    "url": "/SimonTheGame/js/chunk-vendors.39add334.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/SimonTheGame/manifest.json"
+  }
+]);
